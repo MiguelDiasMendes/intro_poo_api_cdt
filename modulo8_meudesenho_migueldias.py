3c2864b6450657d38840e1261b24061a88754ed4
MiguelDiasMendes <migueldiasmendes9@gmail.com>
2026-08-18 16:45:11 -0300

    UPDATE_NEW_PROJECT
    primeiros passos

---
+++ modulo8_meudesenho_migueldias.py
@@ -0,0 +1,32 @@
+'''
+hora de aventura - finn
+gumball - gumball
+ben 10 - benjmim tennyson
+'''
+
+class meninoheroirelogio: 
+
+    def __init__(self,nome,dono,frase_de_efeito):
+        self.nome = nome
+        self.dono = dono
+        self.frase_de_efeito = frase_de_efeito
+
+ben10 = meninoheroirelogio(
+
+    nome = "Benjamim Tennyson",
+    dono = "Vô Max",
+    frase_de_efeito = "hora de virar heroi"
+)
+
+gwentennyson = meninoheroirelogio(
+    nome = "Gwen tennyson",
+    dono = "Vô Max",
+    frase_de_efeito = "Pronto pra desistir?"
+)
+
+VôMax = meninoheroirelogio(
+    nome = "Max Tennyson", 
+    dono = "???",
+    frase_de_efeito = "Eu sou apenas um encanador"
+
+)
