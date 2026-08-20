d4d9cdfac2782edb05347e20d87bd7a365642495
leandrocarmoxav-code <migueldiasmendes9@gmail.com>
2026-08-20 17:02:50 -0300

    update_ben10

--- modulo8_meudesenho_migueldias.py
+++ modulo8_meudesenho_migueldias.py
@@ -30,3 +30,14 @@ VôMax = meninoheroirelogio(
     frase_de_efeito = "Eu sou apenas um encanador"
 
 )
+
+KevinLevin = meninoheroirelogio(
+    nome = "Kevin Levin",
+    dono = "Gwen",
+    frase_de_efeito = "Você nunca vai me vencer, porque você é o mocinho. E mocinhos nunca têm coragem de acabar com as pessoas feito eu!"
+
+)
+
+
+print(f'Nome: {ben10.nome} | {ben10.dono} | {ben10.frase_de_efeito}')
+print(f'Nome: {VôMax.nome} | {VôMax.dono} | {VôMax.frase_de_efeito}')
