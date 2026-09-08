584f7a4e1b4f2812e1d4b8c41f529cf38f3de8dd
leandrocarmoxav-code <migueldiasmendes9@gmail.com>
2026-09-08 16:58:50 -0300

    update_09laulau

---
+++ modulo011_meubancodedados_migueldias.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect("alls-tools.db")
+cursor = conn.cursor()
+
+
+cursor.execute(
+    '''
+    CREATE TABLE  IF NOT EXISTS tasks (
+        id  INTEGRAL PRIMARY KEY AUTOINCREMENT,
+        task_name TEXT NOT NULL,
+        data_fish NUMBER NOT NULL,
+        data_star NUMBER NOT NULL,
+        tarefa_about  STRING NOT NULL)
+'''
+)
+
+conn.commit
